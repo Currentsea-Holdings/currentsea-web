7a2a3cd06aeab243b5d694b462fd7936e717520d
Jo Poblete <joven@poblete.dev>
2023-12-05 21:01:11 -0500

    Replace React+Vite default template to CurrentSea placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+import currentseaLogo from './assets/currentsea-logo.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://currentsea.com/" target="_blank" rel="noreferrer">
+          <img src={currentseaLogo} className="logo" alt="CurrentSea logo" />
         </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => { setCount((count) => count + 1); }}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Placeholder
       </p>
     </>
   )
