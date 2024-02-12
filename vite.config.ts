13e3f01f6875b1a53e298bcd54f2c725ce00b2db
Jo Poblete <joven@poblete.dev>
2024-02-11 22:24:00 -0500

    Update vite config base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/currentsea-web/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'), // Sets '@' to point to the 'src'
