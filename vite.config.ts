0b53fa707cd5812c0cdd6f2bcfa0984653151202
Jo Poblete <joven@poblete.dev>
2024-03-07 15:22:30 -0500

    Install vite-plugin-checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import eslint from 'vite-plugin-eslint';
+import checker from 'vite-plugin-checker';
 import { resolve } from 'path';
 
 // https://vitejs.dev/config/
@@ -10,5 +11,5 @@ export default defineConfig({
       '@': resolve(__dirname, './src'), // Sets '@' to point to the 'src'
     },
   },
-  plugins: [react(), eslint()],
+  plugins: [react(), eslint(), checker({ typescript: true })],
 });
