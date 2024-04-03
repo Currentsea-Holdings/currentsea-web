6d2b5924f6e9f574d317c20840081dfb2bb3321a
Jo <joven@poblete.dev>
2024-04-03 13:43:00 -0400

    Add states.constants

---
+++ src/utils/constants/error-messages.constants.ts
@@ -0,0 +1,9 @@
+export const ERROR_MESSAGES = {
+  INVALID_CREDENTIALS: 'Incorrect email or password. Please try again.',
+  EMAIL_ALREADY_EXISTS:
+    'This email is already in use. If this is your email, please log in or reset your password.',
+  EMAIL_NOT_FOUND: 'Email not found. Please try again.',
+  INVALID_VERIFICATION_CODE: 'Invalid verification code. Please try again.',
+  GENERAL_ERROR: 'Something went wrong. Please try again.',
+  USER_NOT_FOUND: 'User not found. Please try again.',
+};
