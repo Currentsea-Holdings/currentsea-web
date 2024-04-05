a987e8e59821a8277ed0150898c8c0845336ec90
Joven Poblete <joven@poblete.dev>
2024-04-04 20:33:56 -0400

    Refine back button (#31)
    * Update  back button color based on updated mockup
    
    * Add more clarity to BackButton

--- src/components/common/BackButton.tsx
+++ src/components/common/BackButton.tsx
@@ -1,8 +1,14 @@
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Icons from '@/assets/icons';
 
 export type BackButtonProps = {
   className?: string;
+  /**
+   * The route for navigation. Accepts:
+   * - A string for the path to navigate to using `<Link>`.
+   * - A number to navigate steps in history (e.g., -1 for back) using `useNavigate`.
+   * Defaults to -1, navigating back one page.
+   */
   route?: string | number;
 };
 
@@ -17,20 +23,25 @@ export const BackButton = ({ className, route = -1 }: BackButtonProps) => {
     ?.split(' ')
     .filter((className) => !className.startsWith('text-'))
     .join(' ');
-    
-  const handleNavigate = () => {
-    if (typeof route === 'string') {
-      navigate(route);
-    } else if (typeof route === 'number') {
-      navigate(route);
-    }
-  }
-  return (
+
+  return typeof route === 'string' ? (
+    <div className={buttonClasses}>
+      <Link
+        to={route}
+        title="Go back"
+        aria-label="Go back"
+      >
+        <Icons.LeftArrowIcon className={iconClasses} />
+      </Link>
+    </div>
+  ) : (
     <button
       className={buttonClasses}
       title="Go back"
       aria-label="Go back"
-      onClick={handleNavigate}
+      onClick={() => {
+        navigate(route);
+      }}
     >
       <Icons.LeftArrowIcon className={iconClasses} />
     </button>
