ff1f8f7bbef0be4fe8890c918c6c85a476876719
Jo <joven@poblete.dev>
2024-04-04 20:27:23 -0400

    Add more clarity to BackButton

--- src/components/common/BackButton.tsx
+++ src/components/common/BackButton.tsx
@@ -3,6 +3,12 @@ import Icons from '@/assets/icons';
 
 export type BackButtonProps = {
   className?: string;
+  /**
+   * The route for navigation. Accepts:
+   * - A string for the path to navigate to using `<Link>`.
+   * - A number to navigate steps in history (e.g., -1 for back) using `useNavigate`.
+   * Defaults to -1, navigating back one page.
+   */
   route?: string | number;
 };
 
@@ -18,28 +24,24 @@ export const BackButton = ({ className, route = -1 }: BackButtonProps) => {
     .filter((className) => !className.startsWith('text-'))
     .join(' ');
 
-  const handleNavigate = () => {
-    if (typeof route === 'number') {
-      navigate(route);
-    }
-  };
-
   return typeof route === 'string' ? (
-    <Link
-      to={route}
-      title="Go back"
-      aria-label="Go back"
-    >
-      <div className={buttonClasses}>
+    <div className={buttonClasses}>
+      <Link
+        to={route}
+        title="Go back"
+        aria-label="Go back"
+      >
         <Icons.LeftArrowIcon className={iconClasses} />
-      </div>
-    </Link>
+      </Link>
+    </div>
   ) : (
     <button
       className={buttonClasses}
       title="Go back"
       aria-label="Go back"
-      onClick={handleNavigate}
+      onClick={() => {
+        navigate(route);
+      }}
     >
       <Icons.LeftArrowIcon className={iconClasses} />
     </button>
