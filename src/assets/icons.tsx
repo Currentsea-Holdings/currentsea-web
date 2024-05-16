02823290faac6977a999a0f80a1e233bff1dadea
Yashaswini Golla <ygolla@uncc.com>
2024-05-15 22:29:04 -0400

    Campaign Progress Tracker

--- src/assets/icons.tsx
+++ src/assets/icons.tsx
@@ -323,6 +323,58 @@ export const NotificationIcon = ({ className = '' }: IconProps = {}) => {
   );
 };
 
+export const WaitingTimeIcon = ({ className = '' }: IconProps = {}) => {
+  return (
+    <svg
+      width="12px"
+      height="12px"
+      viewBox="-4.32 -4.32 56.64 56.64"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="#000000"
+    >
+      <g
+        id="SVGRepo_bgCarrier"
+        strokeWidth="0"
+      ></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        stroke="#CCCCCC"
+        strokeWidth="0.288"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        {' '}
+        <title>time-solid</title>{' '}
+        <g
+          id="Layer_2"
+          data-name="Layer 2"
+        >
+          {' '}
+          <g
+            id="invisible_box"
+            data-name="invisible box"
+          >
+            {' '}
+            <rect
+              width="48"
+              height="48"
+              fill="none"
+            ></rect>{' '}
+          </g>{' '}
+          <g
+            id="icons_Q2"
+            data-name="icons Q2"
+          >
+            {' '}
+            <path d="M24,2A22,22,0,1,0,46,24,21.9,21.9,0,0,0,24,2ZM35.7,31A2.1,2.1,0,0,1,34,32a1.9,1.9,0,0,1-1-.3L22,25.1V14a2,2,0,0,1,4,0v8.9l9,5.4A1.9,1.9,0,0,1,35.7,31Z"></path>{' '}
+          </g>{' '}
+        </g>{' '}
+      </g>
+    </svg>
+  );
+};
+
 export const SettingsIcon = ({ className = '' }: IconProps = {}) => {
   return (
     <svg
@@ -375,6 +427,7 @@ const Icons = {
   InstagramIcon,
   NotificationIcon,
   SettingsIcon,
+  WaitingTimeIcon,
 };
 
 export default Icons;
